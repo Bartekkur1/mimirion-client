0b9ef6bca058ceaafecbc0b1e46a30e1e8207448
bartekkur1 <bartekkur1@gmail.com>
2022-03-06 18:12:13 +0100

    Changed get config returned type to generic

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,4 @@ export function init(): Promise<void>;
  * Use generic parameter only in case of getting array
  * @param path to configuration variable
  */
-export function get<T>(path: string): string | number | Object | Array<T> | boolean | null;
+export function get<T>(path: string): T;
